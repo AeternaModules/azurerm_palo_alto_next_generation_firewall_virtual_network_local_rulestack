a91dbe998675e2679c58e23d899b6d4f6c776645
Daniel Allen <dan@aeternagroup.net>
2026-01-11 17:04:01 +0000

    Update to schema version v4.57.0

---
+++ variables.tf
@@ -0,0 +1,69 @@
+variable "palo_alto_next_generation_firewall_virtual_network_local_rulestacks" {
+  description = <<EOT
+Map of palo_alto_next_generation_firewall_virtual_network_local_rulestacks, attributes below
+Required:
+    - name
+    - resource_group_name
+    - rulestack_id
+    - network_profile (block):
+        - egress_nat_ip_address_ids (optional)
+        - public_ip_address_ids (required)
+        - trusted_address_ranges (optional)
+        - vnet_configuration (required, block):
+            - trusted_subnet_id (optional)
+            - untrusted_subnet_id (optional)
+            - virtual_network_id (required)
+Optional:
+    - marketplace_offer_id
+    - plan_id
+    - tags
+    - destination_nat (block):
+        - backend_config (optional, block):
+            - port (required)
+            - public_ip_address (required)
+        - frontend_config (optional, block):
+            - port (required)
+            - public_ip_address_id (required)
+        - name (required)
+        - protocol (required)
+    - dns_settings (block):
+        - dns_servers (optional)
+        - use_azure_dns (optional)
+EOT
+
+  type = map(object({
+    name                 = string
+    resource_group_name  = string
+    rulestack_id         = string
+    marketplace_offer_id = optional(string, "pan_swfw_cloud_ngfw")
+    plan_id              = optional(string, "panw-cloud-ngfw-payg")
+    tags                 = optional(map(string))
+    network_profile = object({
+      egress_nat_ip_address_ids = optional(list(string))
+      public_ip_address_ids     = list(string)
+      trusted_address_ranges    = optional(list(string))
+      vnet_configuration = object({
+        trusted_subnet_id   = optional(string)
+        untrusted_subnet_id = optional(string)
+        virtual_network_id  = string
+      })
+    })
+    destination_nat = optional(object({
+      backend_config = optional(object({
+        port              = number
+        public_ip_address = string
+      }))
+      frontend_config = optional(object({
+        port                 = number
+        public_ip_address_id = string
+      }))
+      name     = string
+      protocol = string
+    }))
+    dns_settings = optional(object({
+      dns_servers   = optional(list(string))
+      use_azure_dns = optional(bool, false)
+    }))
+  }))
+}
+
