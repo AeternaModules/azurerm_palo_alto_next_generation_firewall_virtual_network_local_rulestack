68398a471537714ceeb34c5524183be843824875
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks" {
-  description = "All palo_alto_next_generation_firewall_virtual_network_local_rulestack resources"
-  value       = azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks
-}
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_destination_nat" {
-  description = "List of destination_nat values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.destination_nat]
+  description = "Map of destination_nat values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.destination_nat }
 }
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_dns_settings" {
-  description = "List of dns_settings values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.dns_settings]
+  description = "Map of dns_settings values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.dns_settings }
 }
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_marketplace_offer_id" {
-  description = "List of marketplace_offer_id values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.marketplace_offer_id]
+  description = "Map of marketplace_offer_id values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.marketplace_offer_id }
 }
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_name" {
-  description = "List of name values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.name]
+  description = "Map of name values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.name }
 }
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_network_profile" {
-  description = "List of network_profile values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.network_profile]
+  description = "Map of network_profile values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.network_profile }
 }
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_plan_id" {
-  description = "List of plan_id values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.plan_id]
+  description = "Map of plan_id values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.plan_id }
 }
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_resource_group_name" {
-  description = "List of resource_group_name values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.resource_group_name]
+  description = "Map of resource_group_name values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.resource_group_name }
 }
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_rulestack_id" {
-  description = "List of rulestack_id values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.rulestack_id]
+  description = "Map of rulestack_id values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.rulestack_id }
 }
 output "palo_alto_next_generation_firewall_virtual_network_local_rulestacks_tags" {
-  description = "List of tags values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
-  value       = [for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : v.tags]
+  description = "Map of tags values across all palo_alto_next_generation_firewall_virtual_network_local_rulestacks, keyed the same as var.palo_alto_next_generation_firewall_virtual_network_local_rulestacks"
+  value       = { for k, v in azurerm_palo_alto_next_generation_firewall_virtual_network_local_rulestack.palo_alto_next_generation_firewall_virtual_network_local_rulestacks : k => v.tags }
 }
 
